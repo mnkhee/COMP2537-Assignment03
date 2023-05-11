89898f38f03203d6a292b580a64c930e6f700349
Joshua Chuah <joshua2004cc@gmail.com>
2023-05-10 21:36:13 -0700

    Add functionality to card buttons

--- index.js
+++ index.js
@@ -38,6 +38,41 @@ const setup = async () => {
     const numPages = Math.ceil(pokemonList.length / PAGE_SIZE)
     updatePaginationDiv(currentPage, numPages)
 
+    $('body').on('click', '.pokeCard', async function (e) {
+        const pokemonName = $(this).attr('pokeName')
+        const res = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
+        const types = res.data.types.map((type) => type.type.name)
+        $('.modal-body').html(`
+      <div style="width:200px">
+        <img src="${res.data.sprites.other['official-artwork'].front_default}" alt="${res.data.name}"/>
+        <div>
+          <h3>Abilities</h3>
+          <ul>
+            ${res.data.abilities.map((ability) => `<li>${ability.ability.name}</li>`).join('')}
+          </ul>
+        </div>
+
+        <div>
+          <h3>Stats</h3>
+          <ul>
+            ${res.data.stats.map((stat) => `<li>${stat.stat.name}: ${stat.base_stat}</li>`).join('')}
+          </ul>
+        </div>
+
+      </div>
+      <h3>Types</h3>
+      <ul>
+        ${types.map((type) => `<li>${type}</li>`).join('')}
+
+          </ul>
+      
+        `)
+        $('.modal-title').html(`
+        <h2>${res.data.name.toUpperCase()}</h2>
+        <h5>${res.data.id}</h5>
+        `)
+    })
+
 }
 
 $(document).ready(setup)
