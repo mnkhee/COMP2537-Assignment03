c2560804dcdf1d3443e6db4ce51421bac432b718
Joshua Chuah <joshua2004cc@gmail.com>
2023-05-10 21:36:01 -0700

    Add working cards to index.js

--- index.js
+++ index.js
@@ -2,3 +2,42 @@ const PAGE_SIZE = 10
 let currentPage = 1;
 let pokemonList = []
 
+function titleCase(str) {
+    str = str.toLowerCase().split(' ');
+    for (var i = 0; i < str.length; i++) {
+        str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1);
+    }
+    return str.join(' ');
+}
+
+
+const paginate = async (currentPage, PAGE_SIZE, pokemons) => {
+    selected_pokemons = pokemons.slice((currentPage - 1) * PAGE_SIZE, currentPage * PAGE_SIZE)
+
+    $('#pokeCards').empty()
+    selected_pokemons.forEach(async (pokemon) => {
+        const res = await axios.get(pokemon.url)
+        $('#pokeCards').append(`
+      <div class="pokeCard card" pokeName=${res.data.name}   >
+        <h3>${titleCase(res.data.name)}</h3> 
+        <img src="${res.data.sprites.front_default}" alt="${res.data.name}"/>
+        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#pokeModal">
+          More
+        </button>
+      </div>  
+    `)
+    })
+}
+
+const setup = async () => {
+    $('#pokeCards').empty()
+    let response = await axios.get('https://pokeapi.co/api/v2/pokemon?offset=0&limit=810');
+    pokemonList = response.data.results;
+
+    paginate(currentPage, PAGE_SIZE, pokemonList)
+    const numPages = Math.ceil(pokemonList.length / PAGE_SIZE)
+    updatePaginationDiv(currentPage, numPages)
+
+}
+
+$(document).ready(setup)
